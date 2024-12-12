065d80a75d8a85b845fa980de22cfecb256e0be8
Sanketmehta1724 <166901607+Sanketmehta1724@users.noreply.github.com>
2024-12-12 13:31:07 +0530

    Add files via upload

---
+++ String_function.py
@@ -0,0 +1,13 @@
+def printfun(string):
+    print("Length is ",len(string))
+    print("uppercase is ",string.upper())
+    print("lowercase is ",string.lower())
+    print("title is ",string.title())
+    print("capitalise is ",string.capitalize())
+    print("islower ",string.islower())
+    print("isupper ",string.isupper())
+    print("isalpha ",string.isalpha())
+    print("isdigit ",string.isdigit())
+    print("isnum ",string.isnum())
+    #wap to make a module which contains all the basic function related to string and import that module in
+#another file and use that funtionwith string given by a user
